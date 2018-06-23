df398b828f6f5b44a5fd10d87e8d147e26342d0c
Justin Headley <headley.justin@gmail.com>
2018-06-23 00:32:10 -0700

    - Formatted code - Added swaggerHost to prevent issues

--- api.js
+++ api.js
@@ -1,43 +1,42 @@
-'use strict';
-
-let Hapi = require('hapi');
-let mongoose = require('mongoose');
-let RestHapi = require('rest-hapi');
+let Hapi = require('hapi')
+let mongoose = require('mongoose')
+let RestHapi = require('rest-hapi')
 
 async function api () {
-    try {
+  try {
 
-        let server = new Hapi.Server({ port: 8080});
+    let server = new Hapi.Server({port: 8080})
 
-        let config = {
-            appTitle: "rest-hapi-demo",
-            enableTextSearch: true,
-            logRoutes: true,
-            docExpansion: 'list',
-            mongo: {
-                URI: 'mongodb://localhost:27017/rest_hapi'
-            }
-        };
-
-      RestHapi.config = config;
-
-        await server.register({
-                plugin: RestHapi,
-                options: {
-                    mongoose: mongoose,
-                  config: config
-                }
-            });
-
-        await server.start();
-
-      RestHapi.logUtil.logActionComplete(RestHapi.logger, "Server Initialized", server.info);
-
-        return server;
-    } catch (err) {
-        console.log("Error starting server:", err);
+    let config = {
+      appTitle: 'rest-hapi-demo',
+      enableTextSearch: true,
+      logRoutes: true,
+      docExpansion: 'list',
+      swaggerHost: 'localhost:8080',
+      mongo: {
+        URI: 'mongodb://localhost:27017/rest_hapi',
+      },
     }
+
+    RestHapi.config = config
+
+    await server.register({
+      plugin: RestHapi,
+      options: {
+        mongoose: mongoose,
+        config: config,
+      },
+    })
+
+    await server.start()
+
+    RestHapi.logUtil.logActionComplete(RestHapi.logger, 'Server Initialized', server.info)
+
+    return server
+  } catch (err) {
+    console.log('Error starting server:', err)
+  }
 
 }
 
-module.exports = api();
+module.exports = api()
